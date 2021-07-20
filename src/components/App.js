e3a37f00e3980fe350c555c48948e098691a46ee
Jennifer Partida <jpartida@baxterplanning.com>
2021-07-19 23:21:46 -0500

    Connect app to store and fix typo in filename

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux'
+import { handleInitialData } from "../actions/shared";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Header
+        </header>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default connect()(App);
