7cc6c1a18ad18ec2595122982e4dd5fe46ff6fa8
Jennifer Partida <jpartida@baxterplanning.com>
2021-07-30 10:06:58 -0500

    Get questions asked/answered and pass to component props

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Leaderboard extends Component {
+
   render() {
     console.log('LEADERBOARD PROPS', this.props)
+    const { questionsAsked, questionsAnswered, users } = this.props
+
+    debugger
     return(
-      <div>
+      <div className='leaderboard'>
         Leaderboard
       </div>
     )
@@ -13,9 +17,30 @@ class Leaderboard extends Component {
 }
 
 function mapStateToProps({questions, users}) {
+  const userNames = Object.entries(users).map(([userName, _ ]) => userName )
+  const questionsAsked = []
+  Object.entries(users).map(([userName, userValues]) => {
+    questionsAsked.push({[userName]: userValues.questions.length})
+  })
+
+  const questionsAnswered = []
+  userNames.forEach(function(user) {
+    let answered = 0
+    Object.entries(questions).map(([qid, qidValue]) => {
+      if (qidValue.optionOne.votes.includes(user)) {
+        answered += 1
+      }
+      if (qidValue.optionTwo.votes.includes(user)) {
+        answered += 1
+      }
+    });
+    questionsAnswered.push({[user]: answered})
+  })
+
   return {
-    questions,
-    users
+    users,
+    questionsAsked,
+    questionsAnswered
   }
 }
 export default connect(mapStateToProps)(Leaderboard)
